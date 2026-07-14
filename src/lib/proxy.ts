d05d2bfd04b0cb163efc5d0c4c82299e714532cd
Msayeem <msayeem223@gmail.com>
2026-07-15 02:01:08 +0600

    Added Not found page

---
+++ src/lib/proxy.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { headers } from 'next/headers'
+import { auth } from './auth';
+ 
+// This function can be marked `async` if using `await` inside
+export async function proxy(request: NextRequest) {
+const session=await auth.api.getSession({
+    headers:await headers()
+});
+
+if(!session){
+  return NextResponse.redirect(new URL('/login', request.url))
+
+}
+
+}
+ 
+export const config = {
+  matcher: [ '/add-product', '/manage', '/manage/:path*', '/cart'],
+}
